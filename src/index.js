71e1f3699cc3bcc4898a5b1b1b4f8e01d2eace7e
Renzo Sebastian Cavero Canchan <renzo.sebastian.cavero.canchan@gmail.com>
2023-02-16 16:30:02 -0500

    hola

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import LoginPage from './Login/LoginPage';
 import MainPage from './MainPage/MainPage';
-import Req2 from './Req2/Req2';
 import Req4 from './Req4/Req4';
 import Restaurant from './Restaurantes/restaurant';
 import Estado_pedido from './Restaurantes/estado_pedido';
 import Registro_pedido from './Restaurantes/registrar_pedido';
 import Recomendaciones from './recomendaciones/Recomendaciones';
 import './index.css';
+import RESTAURANTES from './Req2/RestaurantesPorCategoria';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -23,7 +23,9 @@ root.render(
         <Route path='/Carrito' element={ <carrito/> } />
         <Route path='/Restaurantes/restaurant' element={ <Restaurant/> } />
         <Route path= '/Restaurantes/estado_pedido' element={ <Estado_pedido/>}/>
-        <Route path='/Req2' element = {<Req2/>}/>
+
+        <Route path='/Req2' element = {<RESTAURANTES/>}/>
+
         <Route path='/Req4' element = {<Req4/>}/>
         <Route path='/Restaurantes/registro_pedido' element = {<Registro_pedido/>}/>
         <Route path='/recomendaciones/recomendaciones' element={<Recomendaciones/>}/>
