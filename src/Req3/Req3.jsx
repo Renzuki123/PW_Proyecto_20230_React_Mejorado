80c1d5f149d43fe79414afc3243dec6d8d89bb0d
Renzo Sebastian Cavero Canchan <renzo.sebastian.cavero.canchan@gmail.com>
2023-02-21 02:06:20 -0500

    Cambios Front

--- src/Req3/Req3.jsx
+++ src/Req3/Req3.jsx
@@ -3,33 +3,34 @@ import './Req3.css'
 import data from './data.js';
 import ArticleList from './ArticleList';
 import ButtonList from './ButtonList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Req3 = () => {
+
     //En data cada objeto tiene su categoria, asi que la iteraremos con Math y guardaremos la categoría en un arreglo
-    const allCategories = [ 'All',...new Set(data.map(article => article.category)) ]
+    const allCategories = ['All', ...new Set(data.map(article => article.category))]
 
     //Usaremos 2 estados en esta aplicacion
     const [categories, setcategories] = useState(allCategories)
     const [articles, setArticles] = useState(data)
 
     const filterCategory = (category) => {
-		if (category === 'All'){
-			setArticles(data)
-			return
-		}
+        if (category === 'All') {
+            setArticles(data)
+            return
+        }
         //Filtra todo lo que es article.category de la categoria elegida por el usuario
-		const filteredData = data.filter(article => article.category === category);
-		setArticles(filteredData)
-	}
+        const filteredData = data.filter(article => article.category === category);
+        setArticles(filteredData)
+    }
 
 
-    console.log(allCategories)
 
     return (
         <>
-            <ButtonList categories={categories} filterCategory={filterCategory}/>
-            <ArticleList articles={articles}/>
+            <ButtonList categories={categories} filterCategory={filterCategory} />
+            <ArticleList articles={articles} />
         </>
     );
 };
