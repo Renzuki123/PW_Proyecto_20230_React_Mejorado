f3e80e873433c4883173962d5f611180d754f049
Renzuki123 <renzosaucedo@hotmail.com>
2023-02-28 19:37:17 -0500

    ya se puede cambiar el estado del pedido a en preparación por parte del modulo Restaurante

---
+++ src/Restaurantes/ver_pedido.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import TopNav from '../Global/TopNav';
+import { Link } from 'react-router-dom';
+
+/*
+const verPedido = () => {
+    const [pedidos, setPedidos] = useState([
+        { id: 1, nombre: "Renzo", plato: "fideos", direccion: "Pueblo Libre", estado: "en preparación", codigo: "ABC123" },
+        { id: 2, nombre: "Juan", plato: "pizza", direccion: "Jesus Maria", estado: "en preparación", codigo: "DEF456" },
+        { id: 3, nombre: "Roberto", plato: "ensalada", direccion: "Los Olivos", estado: "en preparación", codigo: "GHI789" }
+    ]);
+
+    return (
+        <div>
+            <center><h1>Estado del Pedido</h1></center>
+            <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Plato</th>
+                <th>Direccion</th>
+                <th>Estado</th>
+                <th>Código</th>
+            </tr>
+            </thead>
+            <tbody>
+                {pedidos.map(pedido => (
+                    <tr key={pedido.id}>
+                        <td>{pedido.id}</td>
+                        <td>{pedido.nombre}</td>
+                        <td>{pedido.plato}</td>
+                        <td>{pedido.direccion}</td>
+                        <td>{pedido.estado}</td>
+                        <td>{pedido.codigo}</td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+    );
+};
+*/
+function EstadoPedido() {
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPedidos() {
+      const response = await fetch('http://localhost:8000/endpoints/pedidos');
+      const data = await response.json();
+      setPedidos(data.pedidos);
+    }
+    fetchPedidos();
+  }, []);
+
+  const cambiarEstadoPedido = (id, nuevoEstado) => {
+    fetch('http://127.0.0.1:8000/endpoints/cambiarEstado', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id: id, estado: nuevoEstado})
+    })
+    .then(response => {
+      console.log(response);
+      setPedidos(pedidos.map(pedido => {
+        if (pedido.id === id) {
+          return {
+            ...pedido,
+            estado: nuevoEstado
+          };
+        }
+        return pedido;
+      }));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  
+  return (
+    <div>
+      <TopNav category ={4}/>
+      <center><h1>Estado de pedidos</h1></center>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Detalles</th>
+            <th>Dirección</th>
+            <th>Método de pago</th>
+            <th>Código</th>
+            <th>Estado</th>
+            <th>Total</th>
+            <th>Cambiar Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pedidos.map(pedido => (
+            <tr key={pedido.id}>
+              <td>{pedido.id}</td>
+              <td>{pedido.nombre}</td>
+              <td>{pedido.detalles}</td>
+              <td>{pedido.direccion}</td>
+              <td>{pedido.metodo}</td>
+              <td>{pedido.codigo}</td>
+              <td>{pedido.estado}</td>
+              <td>{pedido.total}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    cambiarEstadoPedido(pedido.id, 'EP');
+                  }}
+                >
+                  En preparación
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default EstadoPedido;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Estado_pedido from './Restaurantes/estado_pedido';
 import Entrega_pedido from './Restaurantes/registrar_entrega';
 import Registro_pedido from './Restaurantes/registrar_pedido';
 import Recomendaciones from './recomendaciones/Recomendaciones';
+import Ver_Pedido from './Restaurantes/ver_pedido'
 import './index.css';
 import Req3 from './Req3/Req3';
 import Req4 from './Req4/Req4';
@@ -25,13 +26,13 @@ root.render(
         <Route path='/Carrito' element={ <carrito/> } />
         <Route path='/Restaurantes/restaurant' element={ <Restaurant/> } />
         <Route path= '/Cliente/estado_pedido' element={ <Estado_pedido/>}/>
-
         <Route path='/Req3' element = {<Req3/>}/>
         <Route path='/Req4' element = {<Req4/>}/>
 
         <Route path='/Restaurantes/registrar_entrega' element = {<Entrega_pedido/>}/>
         <Route path='/Restaurantes/registro_pedido' element = {<Registro_pedido/>}/>
         <Route path='/recomendaciones/recomendaciones' element={<Recomendaciones/>}/>
+        <Route path='/Restaurantes/Ver_pedido' element = {<Ver_Pedido/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
