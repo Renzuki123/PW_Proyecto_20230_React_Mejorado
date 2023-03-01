087cdb66091fdc90ec0d8f9e958e9e667d11bf3b
Renzuki123 <renzosaucedo@hotmail.com>
2023-03-01 00:12:49 -0500

    Endpoints de Renzo Saucedo terminados, toda mi parte ya está hecha

--- src/Restaurantes/registrar_entrega.jsx
+++ src/Restaurantes/registrar_entrega.jsx
@@ -1,69 +1,7 @@
+/*
 import TopNav from "../Global/TopNav"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-/*
-const DeliveryTable = () => {
-    const [deliveries, setDeliveries] = useState([
-      { id: 1, verificationCode: '123456', status: 'En Preparación' },
-      { id: 2, verificationCode: 'abcdef', status: 'En Preparación' },
-    ]);
-  
-    const [inputValues, setInputValues] = useState({});
-  
-    const handleChangeStatus = (id) => {
-      const updatedDeliveries = deliveries.map((delivery) => {
-        if (delivery.id === id) {
-          delivery.status = delivery.status === 'En Preparación' ? 'Completado' : 'En Preparación';
-        }
-        return delivery;
-      });
-      setDeliveries(updatedDeliveries);
-    };
-  
-    const handleInputChange = (event, id) => {
-      setInputValues({
-        ...inputValues,
-        [id]: event.target.value,
-      });
-    };
-  
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Código de Verificación</th>
-            <th>Estado del Pedido</th>
-          </tr>
-        </thead>
-        <tbody>
-          {deliveries.map((delivery) => (
-            <tr key={delivery.id}>
-              <td>{delivery.id}</td>
-              <td>
-                <input
-                  type="text"
-                  value={inputValues[delivery.id] || ''}
-                  onChange={(event) => handleInputChange(event, delivery.id)}
-                />
-              </td>
-              <td>
-                <input type="text" value={delivery.status} disabled />
-                <button onClick={() => handleChangeStatus(delivery.id)}>
-                  Cambiar estado
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
-  
-  export default DeliveryTable;
-
-*/
 
 function Registrarentrega(){
   const [pedido, setPedido] = useState("")
@@ -89,7 +27,7 @@ function Registrarentrega(){
      
     return <form>
       <label>Pin de pedido</label>
-      <input type="text" id="code" value={code} onChange={(event) => setCode(event.target.value)}/>
+      <input type="text" id="id" value={code} onChange={(event) => setCode(event.target.value)}/>
       <button type="button" onClick={handleSubmit}>Buscar Pedido</button>
     </form>
   }
@@ -103,18 +41,29 @@ function Registrarentrega(){
     if (error === "") {
       if (pedido!== "") {
         return <table>
-        <tr>
-          <th>Cod</th>
-          <th>Detalle</th>
-          <th>Pin de verificación</th>
-        </tr>
-        <tr>
-          <th>{pedido.code}</th>
-          <th>{pedido.desc}</th>
-          <th>{pedido.code_v}</th>
-          <th><button type="submit" onClick={vacio}>Confirmar la entrega</button></th>
-        </tr>
-      </table>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Detalles</th>
+            <th>Dirección</th>
+            <th>Método</th>
+            <th>Código</th>
+            <th>Estado</th>
+            <th>Total</th>
+            <th>Confirmar la entrega</th>
+          </tr>
+          <tr>
+            <td>{pedido.id}</td>
+            <td>{pedido.nombre}</td>
+            <td>{pedido.detalles}</td>
+            <td>{pedido.direccion}</td>
+            <td>{pedido.metodo}</td>
+            <td>{pedido.codigo}</td>
+            <td>{pedido.estado}</td>
+            <td>{pedido.total}</td>
+            <td><button type="button" onClick={vacio}>Confirmar la entrega</button></td>
+          </tr>
+        </table>
       }
     }
     else {
@@ -131,3 +80,140 @@ function Registrarentrega(){
 }
 
 export default Registrarentrega
+
+*/
+
+import TopNav from "../Global/TopNav";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Registrarentrega() {
+  const [pedido, setPedido] = useState("");
+  const [error, setError] = useState("");
+
+  const FormRegistrar = function () {
+    const [code, setCode] = useState("");
+
+    const handleSubmit = async function (event) {
+      const body = {
+        code: code,
+      };
+      const response = await fetch(
+        "http://localhost:8000/endpoints/registrarentrega",
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+        }
+      );
+
+      const data = await response.json();
+      if (data.error === "") {
+        setError(data.error);
+        setPedido(data.producto);
+      } else {
+        setError(data.error);
+      }
+    };
+
+    return (
+      <form>
+        <label>Pin de pedido</label>
+        <input
+          type="text"
+          id="id"
+          value={code}
+          onChange={(event) => setCode(event.target.value)}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Buscar Pedido
+        </button>
+      </form>
+    );
+  };
+
+  const vacio = function () {
+    console.log("Se hizo click");
+    setPedido("");
+    setError("");
+    return <div>holla</div>;
+  };
+
+  const Tabla = function () {
+    if (error === "") {
+      if (pedido !== "") {
+        return (
+          <table>
+            <tbody>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Detalles</th>
+              <th>Dirección</th>
+              <th>Método</th>
+              <th>Código</th>
+              <th>Estado</th>
+              <th>Total</th>
+              <th></th>
+            </tr>
+            <tr>
+            <td>{pedido.id}</td>
+            <td>{pedido.nombre}</td>
+            <td>{pedido.detalles}</td>
+            <td>{pedido.direccion}</td>
+            <td>{pedido.metodo}</td>
+            <td>{pedido.codigo}</td>
+            <td>{pedido.estado}</td>
+            <td>{pedido.total}</td>
+              <td>
+                <button onClick={cambiarEstadoPedido(pedido.id, 'E')}>Confirmar Entrega</button>
+              </td>
+            </tr>
+          </tbody>
+          </table>
+        );
+      } else {
+        return <div>No hay pedidos seleccionados</div>;
+      }
+    } else {
+      return <div>{error}</div>;
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const cambiarEstadoPedido = (id, nuevoEstado) => {
+    fetch('http://127.0.0.1:8000/endpoints/cambiarEstado', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id: id, estado: nuevoEstado})
+    })
+    .then(response => {
+      console.log(response);
+      setPedido(pedido.map(pedido => {
+        if (pedido.id === id) {
+          return {
+            ...pedido,
+            estado: nuevoEstado
+          };
+        }
+        return pedido;
+      }));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  return (
+    <div>
+      <TopNav />
+      <h1>Registrar Entrega</h1>
+      <FormRegistrar />
+      <Tabla />
+    </div>
+  );
+}
+
+export default Registrarentrega;
